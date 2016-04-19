b587a2528abff69886adbda9537158fbdafe4e5d
andyhu0777 <andyhu0777@gmail.com>
2016-04-19 14:43:13 +0000

    add ssservice

--- configure-iptables.sh
+++ configure-iptables.sh
@@ -9,6 +9,7 @@ iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 iptables -A INPUT -p tcp --dport 80 -j ACCEPT
 iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+iptables -A INPUT -p tcp -j ACCEPT
 iptables -P INPUT DROP
 iptables -P FORWARD DROP
 iptables -P OUTPUT ACCEPT
