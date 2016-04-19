523f6237487b4a69dab3b6f80733a5743d096177
andyhu0777 <andyhu0777@gmail.com>
2016-04-19 03:23:05 +0000

    add iptables config

---
+++ configure-iptables.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+
+iptables -P INPUT ACCEPT
+iptables -F
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+iptables -P INPUT DROP
+iptables -P FORWARD DROP
+iptables -P OUTPUT ACCEPT
+
+/sbin/service iptables save
+
+iptables -L -v
